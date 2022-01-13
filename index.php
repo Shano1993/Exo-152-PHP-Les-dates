1c01f1a1c5bfc695bca5afae39c4ca9a1aed63dd
Shano1993 <stefan.hanotiau@gmail.com>
2022-01-13 13:39:27 +0100

    php

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 
 // Premiere ligne
 
+$day = date("D");
+echo "<p>Jour de la semaine, $day</p>";
 
+$date1 = date("Y.m.j");
+echo "<p>Date d'aujourd'hui, $date1</p>";
+
+$hour = date("H:i:s");
+echo "<p>L'heure actuelle est $hour</p>";
 
 
 
